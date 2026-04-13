1cfd6a321d29c4dc687027a3a775fb945d000c80
umavanmoriel <umeda19971102@gmail.com>
2026-04-13 22:23:54 +0200

    --Lade till metoder i TodoList

--- src/main.ts
+++ src/main.ts
@@ -6,24 +6,21 @@ interface Todo {
   priority: 1|2|3;
 }
 
-class TodoList implements Todo {
+class TodoList {
   todos: Array<Todo>;
 
-  constructor(task:string, completed:boolean, priority: 1|2|3) {
-    this.task= task;
-    this.completed= completed;
-    this.priority= priority;
-
+  constructor() {
+    this.todos= [];
     this.loadFromLocalStorage();
   }
 
   addTodo(task: string, priority: number): boolean {
-    if(task: string, priority: 1|2|3) {
-      let newTodo : Todo {
-        task: task;
-        completed: false;
-        priority: priority as 1 || priority as 2 || priority as 3;
-      }
+    if(priority === 1 || priority === 2 || priority === 3) {
+      let newTodo : Todo = {
+        task: task,
+        completed: false,
+        priority: priority as 1 || priority as 2 || priority as 3
+      };
 
       this.todos.push(newTodo);
       this.saveToLocalStorage();
@@ -32,5 +29,29 @@ class TodoList implements Todo {
       return false;
     };
   };
+
+  markTodoCompleted(todoIndex: number): void {
+    if (this.todos[todoIndex]) {
+      this.todos[todoIndex].completed = true;
+      this.saveToLocalStorage();
+    }
+  }
+
+  getTodos(): Todo[] {
+    return this.todos;
+  };
+
+  saveToLocalStorage(): void {
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+
+  loadFromLocalStorage(): void {
+    let savedTodos = localStorage.getItem("todos");
+    if (savedTodos) {
+      this.todos = JSON.parse(savedTodos);
+    } else {
+      this.todos = [];
+    }
+  }
 
 }
