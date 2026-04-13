04ebcc01724ca2a948d8632a189a80797deb9ccd
umavanmoriel <umeda19971102@gmail.com>
2026-04-13 19:22:48 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import './style.css'
+
+interface Todo {
+  task: string;
+  completed: boolean;
+  priority: 1|2|3;
+}
+
+class TodoList implements Todo {
+  todos: Array<Todo>;
+
+  constructor(task:string, completed:boolean, priority: 1|2|3) {
+    this.task= task;
+    this.completed= completed;
+    this.priority= priority;
+
+    this.loadFromLocalStorage();
+  }
+
+  addTodo(task: string, priority: number): boolean {
+    if(task: string, priority: 1|2|3) {
+      let newTodo : Todo {
+        task: task;
+        completed: false;
+        priority: priority as 1 || priority as 2 || priority as 3;
+      }
+
+      this.todos.push(newTodo);
+      this.saveToLocalStorage();
+      return true;
+    } else {
+      return false;
+    };
+  };
+
+  markTodoCompleted(todoIndex: number): void {
+    console.log("Todo klar")
+  }
+
+  getTodos(): Todo[] {
+    return;
+  };
+
+  saveToLocalStorage(): void {
+    localStorage.setItem(Todo[]);
+  }
+
+  loadFromLocalStorage(): void {
+    localStorage.getItem(Todo[]);
+  }
+
+
+}
